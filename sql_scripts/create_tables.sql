a459dd79575bad732fdb51c663378cd82e599de1
egorvorobev <vorobyv.egor@gmail.com>
2024-05-22 00:14:23 +0300

    load season to db and create view

--- sql_scripts/create_tables.sql
+++ sql_scripts/create_tables.sql
@@ -22,5 +22,26 @@ CREATE TABLE api_football_first_load.leagues
   PRIMARY KEY (league_id)
 );
 
+CREATE TABLE api_football_first_load.season_prep
+(
+  league_id int4,
+  seasons text[],
+  load_dttm timestamp,
+  source_id text,
+  PRIMARY KEY (league_id)
+);
+
+CREATE VIEW api_football_first_load.season_v AS
+WITH cte AS (SELECT league_id, unnest(seasons)::jsonb AS season FROM api_football_first_load.season_prep)
+SELECT
+	row_number() over() AS season_id,
+	league_id,
+	(season -> 'year')::text AS YEAR,
+	to_date(replace((season -> 'start')::text, '"', ''), 'yyyy-MM-dd') AS start,
+	to_date(replace((season -> 'end')::text, '"', ''), 'yyyy-MM-dd') AS END,
+	(season -> 'current')::text:: Boolean AS current
+FROM cte;
+
+
 
 
